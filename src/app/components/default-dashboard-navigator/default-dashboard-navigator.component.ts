45da7904868838896e003ac8f7f719e3194068b8
jeziel-nogueira <jezielnogueira_15@yahoo.com.br>
2025-02-24 11:10:28 -0300

    added left bar dropdown

--- src/app/components/default-dashboard-navigator/default-dashboard-navigator.component.ts
+++ src/app/components/default-dashboard-navigator/default-dashboard-navigator.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-default-dashboard-navigator',
   imports: [
+    CommonModule,
     PrimaryButtonComponent
   ],
   templateUrl: './default-dashboard-navigator.component.html',
   styleUrl: './default-dashboard-navigator.component.scss'
 })
 export class DefaultDashboardNavigatorComponent {
+  dropdowns = {
+    bookManager: true,
+    loanControl: false,
+    libraryUsers: false,
+  };
 
+  toggleDropdown(menu: keyof typeof this.dropdowns){
+    console.log(menu)
+    this.dropdowns[menu] = !this.dropdowns[menu]
+  }
 }
